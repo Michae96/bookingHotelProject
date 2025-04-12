c004bcdc038d4c8fa498357ababc3ee9b36613d7
Бакытжан Баяхмет <b.bayakhmet@ovtech.kz>
2025-04-12 19:15:39 +0500

    Added fixed type of room

--- src/main/java/com/example/bookinghotelproject/entity/Room.java
+++ src/main/java/com/example/bookinghotelproject/entity/Room.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
@@ -18,6 +19,7 @@ public class Room {
     private Long id;
 
     @NotBlank(message = "Room type cannot be blank")
+    @Pattern(regexp = "Standard|Premium|Luxury|Budget", message = "Room type must be one of the following: Standard, Premium, Luxury, Budget")
     @Column(nullable = false)
     private String type;
 
@@ -36,6 +38,5 @@ public class Room {
     @ManyToOne
     @JoinColumn(name = "hotel_id", nullable = false)
     @NotNull(message = "Hotel must be specified")
-    @JsonIgnore
     private Hotel hotel;
 }
